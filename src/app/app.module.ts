efb638c0686bb426726d5f29a9082973feddff1e
HalgoBZZ <hafedh.baazouzi@gmail.com>
2019-03-02 22:38:59 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { AsideComponent } from './components/aside/aside.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    BodyComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule
